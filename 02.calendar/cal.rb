7de647766f1cb70676d2fba6f0232aff029f0616
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-14 14:21:02 +0900

    if文の内容を検討中

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,55 +3,54 @@
 require 'date'
 require 'optparse'
 
-params = ARGV.getopts("y:m:")
+params = ARGV.getopts('y:m:')
 
-input_year = params["y"].to_i #入力した年
-input_month = params["m"].to_i #入力した月
+input_year = params['y'].to_i # 入力した年
+input_month = params['m'].to_i # 入力した月
 
 today_date = Date.today
 
 if input_year == 0 && input_month == 0
   manipulated_year = today_date.year # 今年
-  this_month = today_date.month #今月
-  first_day = Date.new(today_date.year,today_date.month,1) #今月1日
-  last_day = Date.new(today_date.year,today_date.month,-1) #今月末日
-elsif input_year == 0 && input_month != 0 
+  this_month = today_date.month # 今月
+  first_day = Date.new(today_date.year, today_date.month, 1) # 今月1日
+  last_day = Date.new(today_date.year, today_date.month, -1) # 今月末日
+elsif input_year == 0 && input_month != 0
   manipulated_year = today_date.year
-  first_day = Date.new(manipulated_year,input_month,1)
-  last_day = Date.new(manipulated_year,input_month,-1)
+  first_day = Date.new(manipulated_year, input_month, 1)
+  last_day = Date.new(manipulated_year, input_month, -1)
 else
-  first_day = Date.new(input_year,input_month,1)
-  last_day = Date.new(input_year,input_month,-1)
+  first_day = Date.new(input_year, input_month, 1)
+  last_day = Date.new(input_year, input_month, -1)
 end
 
 # 1日の曜日を取得する。start_atはカレンダーで「1」が表示される位置になる
 start_at = first_day.wday
 
-#当月の日数
+# 当月の日数
 day_count = last_day.day
 
 # 開始日までの空白(スペース)と当月の日数の合計
 total_days = day_count + start_at
 
 # カレンダー上部のタイトル
-title =  "#{first_day.month}月#{first_day.year}"
+title = "#{first_day.month}月#{first_day.year}"
 print title.to_s.center(22)
 
 # 曜日ラベル
-week_labels= "\n 日 月 火 水 木 金 土 "
+week_labels = "\n 日 月 火 水 木 金 土 "
 puts week_labels # 曜日
 
-#反転させる必要があるか調べるための準備
+# 反転させる必要があるか調べるための準備
 just_today = Date.today.day
-retuen_date = Date.new(first_day.year,first_day.month,just_today)
+retuen_date = Date.new(first_day.year, first_day.month, just_today)
 
-(1..total_days).each do | cal |
+(1..total_days).each do |cal|
   if cal <= start_at
-    print "   "
+    print '   '
   else
     num = cal - start_at # 当月の日付
-    if
-      num == just_today && today_date == retuen_date
+    if num == just_today && today_date == retuen_date
       num = num.to_s.rjust(3)
       print "\e[7m#{num}\e[0m"
     else
@@ -59,10 +58,7 @@ retuen_date = Date.new(first_day.year,first_day.month,just_today)
       print num
     end
   end
-  if cal % 7 == 0
-    print "\n"
-  end
+  print "\n" if cal % 7 == 0
 end
 
 print "\n"
-
