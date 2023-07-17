2658cb5453665d387bc2477b72f04af78ea604a9
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-17 22:54:23 +0900

    出力させるデータ型を変更(string→array)

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -15,14 +15,27 @@ output_month = input_month.zero? ? Date.today.month : input_month
 output_first_date = Date.new(output_year, output_month, 1)
 output_last_date = Date.new(output_year, output_month, -1)
 
-# 1日の曜日を取得する。start_atはカレンダーで「1」が表示される位置になる
-start_at = output_first_date.wday
+# 1日の曜日を取得する。
+start_at = Array.new(output_first_date.wday, "  ")
 
 # 当月の日数
-day_count = output_last_date.day
+day_count = Array.new(output_last_date.day) { |i| i + 1 }
+day_count = day_count.map! { |num| num.to_s.length == 1 ? " #{num}" : num.to_s }
+
+# 出力するデータが今月か調べ、該当する場合は今日の日付の要素を反転させる。
+if output_year == Date.today.year && output_month == Date.today.month
+  just = Date.today.day
+  day_count.map! do |c|
+    if c == day_count[just-1]
+      "\e[7m#{c}\e[0m"
+    else
+     c
+    end
+  end
+end
 
 # 開始日までの空白(スペース)と当月の日数の合計
-total_days = day_count + start_at
+total_days = start_at + day_count
 
 # カレンダー上部のタイトル
 title = "#{output_first_date.month}月#{output_first_date.year}"
@@ -32,24 +45,6 @@ print title.to_s.center(22)
 week_labels = "\n 日 月 火 水 木 金 土 "
 puts week_labels # 曜日
 
-# 反転させる必要があるか調べるための準備
-just_today = Date.today.day
-retuen_date = Date.new(output_first_date.year, output_first_date.month, just_today)
-
-(1..total_days).each do |cal|
-  if cal <= start_at
-    print '   '
-  else
-    num = cal - start_at # 当月の日付
-    if num == just_today && today_date == retuen_date
-      num = num.to_s.rjust(3)
-      print "\e[7m#{num}\e[0m"
-    else
-      num = num.to_s.rjust(3)
-      print num
-    end
-  end
-  print "\n" if cal % 7 == 0
+total_days.each_slice(7) do |slice|
+  puts slice.join(' ')
 end
-
-print "\n"
