99508ccfb86636fe06df2ee896ff31ac452d4667
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-18 15:13:07 +0900

    不要な記述(to_a)を削除し処理を一部変更(map!→map)

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,17 +14,16 @@ output_first_day = Date.new(output_year, output_month, 1)
 output_last_day = Date.new(output_year, output_month, -1)
 
 # 1日の曜日(1日の前に必要なスペースの数)を取得し配列を作成
-day_position_1 = Array.new(output_first_day.wday, "  ")
+day_position_1 = Array.new(output_first_day.wday, '  ')
 
 # 当月の日数
-dairy_date = Array.new(output_last_day.day) { |i| i + 1 }
-dairy_date = dairy_date.map! { |num| num.to_s.length == 1 ? " #{num}" : num }
+dairy_date = (1..output_last_day.day).map { |num| num.to_s.length == 1 ? " #{num}" : num }
 
 # 出力するデータが今月か調べ、該当する場合は今日の日付の要素を反転させる。
 if output_year == Date.today.year && output_month == Date.today.month
   date_for_custom = Date.today.day
-  dairy_date.map! do |day_value|
-    if day_value == dairy_date[date_for_custom-1]
+  dairy_date = dairy_date.map do |day_value|
+    if day_value == dairy_date[date_for_custom - 1]
       "\e[7m#{day_value}\e[0m"
     else
       day_value
@@ -39,9 +38,9 @@ date_for_calender = day_position_1 + dairy_date
 title = "#{output_first_day.month}月#{output_first_day.year}"
 
 # 曜日ラベル
-week_labels = "日 月 火 水 木 金 土"
+week_labels = '日 月 火 水 木 金 土'
 
-puts title.to_s.center(20),week_labels
+puts title.to_s.center(20), week_labels
 
 date_for_calender.each_slice(7) do |week|
   puts week.join(' ')
