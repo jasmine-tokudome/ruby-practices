cc80a475bfa26960972eb27bf658dc848bf63f98
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-18 19:32:09 +0900

    下書き(スコア計算自体は可能/3投目を除く)

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -13,54 +13,31 @@ scores.each do |s|
     shots << s.to_i
   end
 end
-binding.break
+
+p shots
 
 frames = []
 shots.each_slice(2) do |s|
   frames << s
 end
 
-point = 0
-frames.each do |frame|
-  if frame[0] == 10 # strike
-    point += 30
-  elsif frame.sum == 10 # spare
-    point += frame[0] + 10
-  else
-    point += frame.sum
+p frames
+
+add_point = 0
+frames.each_with_index do |n, idx|
+  if n.sum == 10
+     if n.last == 0
+      add_point += frames[1+idx].sum
+     else
+      add_point += frames[1+idx].first
+     end
+    if idx == 8
+      break
+    end
   end
 end
-puts point
 
+frames[10]
 
-
-# (要件)
-# 1ゲーム=10フレーム。
-# 1フレーム=2投（ストライクの場合は1投）。
-# ストライク=1投目で10本倒す。
-# スペア=1投目で全て倒せなかった場合、2投目で全て倒す。
-# ストライクの場合は2投目はなし。
-# スペアは1投目の本数+10点
-# ストライクの場合は30点
-# ありえない投球数やありえない数字・記号がこない前提。
-
-# (追加要件)以下が新ゲーム特有のルール
-# スペアの時は次の１投目まで足す。
-# ストライクのときは次の2頭目まで足す。
-# 10フレーム目で1当メガストライク、2投目がスペアの場合は3頭目まで投げられる。
-
-# 詳しく条件を考える。
-
-# スペアの時は次の１投目まで足す。
-# スペアの時→「elsif frame.sum == 10 」のとき。
-# 次の１投目まで足す。
-# →次の入力が必要(ここが肝??)
-# そのための配列データが有ると良いのかも。
-
-# ストライクのときは次の2頭目まで足す。
-# ストライクのとき→ frame[0] == 10
-# 次の2頭目まで足す。
-# →次のframeまで足す
-
-# 10フレーム目で1当目がストライク、2投目がスペアの場合は3頭目まで投げられる。
-# →ここは前半2つの見通しがたったあとに考える。
+p add_point
+p shots.sum + add_point
