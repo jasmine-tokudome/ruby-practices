7139a360023bd8dfc9c09586f5819dc9156e2538
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-18 22:42:54 +0900

    変数名を変更(daily_date)

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -17,13 +17,13 @@ output_last_day = Date.new(output_year, output_month, -1)
 day_position_1 = Array.new(output_first_day.wday, '  ')
 
 # 当月の日数
-dairy_date = (1..output_last_day.day).map { |num| num.to_s.length == 1 ? " #{num}" : num }
+daily_date = (1..output_last_day.day).map { |num| num.to_s.length == 1 ? " #{num}" : num }
 
 # 出力するデータが今月か調べ、該当する場合は今日の日付の要素を反転させる。
 if output_year == Date.today.year && output_month == Date.today.month
   date_for_custom = Date.today.day
-  dairy_date = dairy_date.map do |day_value|
-    if day_value == dairy_date[date_for_custom - 1]
+  daily_date = daily_date.map do |day_value|
+    if day_value == daily_date[date_for_custom - 1]
       "\e[7m#{day_value}\e[0m"
     else
       day_value
@@ -32,7 +32,7 @@ if output_year == Date.today.year && output_month == Date.today.month
 end
 
 # 開始日までの空白(スペース)と当月の日数の合計
-date_for_calender = day_position_1 + dairy_date
+date_for_calender = day_position_1 + daily_date
 
 # カレンダー上部のタイトル
 title = "#{output_first_day.month}月#{output_first_day.year}"
