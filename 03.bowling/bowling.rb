d9a588dec5edf7f713dcf5953cf0e68e928a6f10
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2023-07-19 16:03:20 +0900

    firstメソッドの使用箇所について[0]指定に置き換える

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-require 'debug'
-
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -23,17 +21,31 @@ end
 
 p frames
 
-add_point = 0
+accumulated_bonus_points = 0
 frames.each_with_index do |n, idx|
   if n.sum == 10
      if n.last == 0
-      add_point += frames[idx+1].sum
+      if frames[idx+1].last == 0 && frames[idx+1].sum == 10 && idx + 2 < 9
+      accumulated_bonus_points += frames[idx+1].first + frames[idx+2].first
+      else
+        accumulated_bonus_points += frames[idx+1].sum
+      end
      else
-      add_point += frames[idx+1].first
+      accumulated_bonus_points += frames[idx+1].first
      end
      break if idx == 8
   end
 end
 
-p add_point
-p shots.sum + add_point
+# 9回と10回の1投目がストライクの場合は、10回目の3投目のスコアを加算する。
+if frames[8].sum == 10 && frames[8].last == 0 && frames[9].sum == 10 && frames[9].last == 0
+  accumulated_bonus_points += frames[10].first
+end
+
+# 9回目まですべてストライクで10回目が0点ではない場合は10点加算する
+if frames.all? { |frame| frame.last == 0 && frame.sum == 10 } && frames[9].sum != 0 
+  accumulated_bonus_points += 10
+end
+
+p accumulated_bonus_points
+p shots.sum + accumulated_bonus_points
