057d5c7aa99734858eb86b8623eadce34a7276ac
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-07-26 12:57:12 +0900

    ブロック引数の変数名を変更(idx→frame)/不要な処理を削除/mapへの変更/

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,38 +3,25 @@
 
 score = ARGV[0]
 scores = score.split(',')
-shots = []
-scores.each do |s|
-  if s == 'X'
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
-  end
-end
 
-frames=shots.each_slice(2).to_a
+shots = scores.map { |s| s == 'X' ? [10, 0] : s.to_i }.flatten
+
+frames = shots.each_slice(2).to_a
 
 accumulated_bonus_points = 0
-frames.each_with_index do |frame, idx|
+frames.each_with_index do |frame, frame_number|
   if frame.sum == 10
     accumulated_bonus_points += if frame.last.zero?
-                                  if frames[idx + 1].last.zero? && frames[idx + 1].sum == 10 && idx + 2 < 9
-                                    frames[idx + 1][0] + frames[idx + 2][0]
+                                  if frames[frame_number + 1].last.zero? && frames[frame_number + 1].sum == 10
+                                    frames[frame_number + 1][0] + frames[frame_number + 2][0]
                                   else
-                                    frames[idx + 1].sum
+                                    frames[frame_number + 1].sum
                                   end
                                 else
-                                  frames[idx + 1][0]
+                                  frames[frame_number + 1][0]
                                 end
   end
-  break if idx == 8
+  break if frame_number == 8
 end
-
-# 9回と10回の1投目がストライクの場合は、10回目の2投目のスコアを加算する。
-accumulated_bonus_points += frames[10].first if frames[8] == [10, 0] && frames[9] == [10, 0]
-
-# 9回目まですべてストライクで10回目が0点ではない場合は10点加算する
-accumulated_bonus_points += 10 if frames.all? { |frame| frame == [10, 0] } && frames[9].sum != 0
 
 puts shots.sum + accumulated_bonus_points
