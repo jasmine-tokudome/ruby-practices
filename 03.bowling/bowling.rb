8f13020808ae25d8c9caca7c7523af56da0e114e
Jasmine <orange_blossom_1684mk@yahoo.co.jp>
2023-07-27 09:48:52 +0900

    flat_mapへの変更と定数PERFECT_SCOREの設定

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,15 +4,17 @@
 score = ARGV[0]
 scores = score.split(',')
 
-shots = scores.map { |s| s == 'X' ? [10, 0] : s.to_i }.flatten
+shots = scores.flat_map { |s| s == 'X' ? [10, 0] : s.to_i }
 
 frames = shots.each_slice(2).to_a
 
+PERFECT_SCORE = 10
+
 accumulated_bonus_points = 0
 frames.each_with_index do |frame, frame_number|
-  if frame.sum == 10
+  if frame.sum == PERFECT_SCORE
     accumulated_bonus_points += if frame.last.zero?
-                                  if frames[frame_number + 1].last.zero? && frames[frame_number + 1].sum == 10
+                                  if frames[frame_number + 1].last.zero? && frames[frame_number + 1].sum == PERFECT_SCORE
                                     frames[frame_number + 1][0] + frames[frame_number + 2][0]
                                   else
                                     frames[frame_number + 1].sum
@@ -21,7 +23,7 @@ frames.each_with_index do |frame, frame_number|
                                   frames[frame_number + 1][0]
                                 end
   end
-  break if frame_number == 8
+  break if frame_number == 8 # ボーナスポイントの加算があるのは9回目までのため。
 end
 
 puts shots.sum + accumulated_bonus_points
